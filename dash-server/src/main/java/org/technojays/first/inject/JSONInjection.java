ddd8dc99f9898b36e8f05ac4a2168e4cba7f9b46
Derelle Redmond <Derelle.Redmond@gmail.com>
2015-11-01 23:02:47 -0500

    [H4] More Hibernate Deprecation

--- dash-server/src/main/java/org/technojays/first/inject/JSONInjection.java
+++ dash-server/src/main/java/org/technojays/first/inject/JSONInjection.java
@@ -1,7 +1,6 @@
 package org.technojays.first.inject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.google.inject.Binder;
 import com.google.inject.Inject;
